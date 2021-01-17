2b64bcfe4fd68d2b2519b57039d50bb01f964fa4
Austyn Herman <austynherman112994@gmail.com>
2021-01-16 19:26:57 -0800

    Doc strings

--- bitcrawler/link_utils.py
+++ bitcrawler/link_utils.py
@@ -1,25 +1,92 @@
-"""TODO: Docstring
+# -*- coding: utf-8 -*-
+"""Provides tools for interacting with links and URLs.
+
 """
 import urllib.parse
 
 
-def is_relative(url):
-    """TODO: Docstring
+def is_relative(link):
+    """Determines if a link is a relative link.
+
+    Args:
+        link (str): A link.
+
+    Returns:
+        bool: True if the link is a relative link. Otherwise False.
+
+    Examples:
+        >>> is_relative("/test/link/path")
+        True
+
+        >>> is_relative("http://python.org/test/link/path")
+        False
+
     """
-    return not bool(urllib.parse.urlparse(url).netloc)
+    return not bool(urllib.parse.urlparse(link).netloc)
+
 
 def get_base_url(url):
-    """TODO: Docstring
+    """Gets the base url (scheme://netloc) from a url.
+
+    Uses the python urllib.parse object to generate the scheme and netloc.
+
+    Args:
+        url (str): A URL.
+
+    Returns:
+        string: The base url generated from the provided url.
+
+    Examples:
+        >>> get_base_url("http://python.org:8000/test/link/path")
+        "http://python.org:8000"
+
     """
     url_obj = urllib.parse.urlparse(url)
     base_url = f"{url_obj.scheme}://{url_obj.netloc}"
     return base_url
 
-def is_same_domain(url1, url2):
-    """TODO: Docstring
+
+def get_domain(url):
+    """Checks is two urls share the same domain.
+
+    Generates domains from the urllib.parse objects netloc. The domain is
+    extracted from the netloc by stripping the port and subdomain info.
+
+    Args:
+        url (str): A URL.
+
+    Returns:
+        str: The domain of from the input URL.
+
+    Examples:
+        >>> get_domain("http://subdomain.python.org:8000/test/link/path")
+        "python.org"
+
     """
-    url1_netloc = urllib.parse.urlparse(url1).netloc
-    url2_netloc = urllib.parse.urlparse(url2).netloc
-    url1_domain = ".".join(url1_netloc.split(".")[-2:]).split(":")[0]
-    url2_domain = ".".join(url2_netloc.split(".")[-2:]).split(":")[0]
-    return url1_domain == url2_domain
+    url_netloc = urllib.parse.urlparse(url1).netloc
+    url_domain = ".".join(url1_netloc.split(".")[-2:]).split(":")[0]
+    return url_domain
+
+
+def is_same_domain(url1, url2):
+    """Checks is two urls share the same domain.
+
+    Uses `get_domain` to extract a domain.
+
+    Args:
+        url1 (str): The first URL for comparison.
+        url2 (str): The second URL for comparison.
+
+    Returns:
+        bool: True if the domains match. Otherwise False.
+
+    Examples:
+
+        >>>is_same_domain("http://python.org", "https://subdomain.python.org:8000")
+        True
+
+        >>>is_same_domain("http://python.org", "https://pandas.com")
+        False
+
+    """
+    return get_domain(url1) == get_domain(url2)
--- bitcrawler/parsing.py
+++ bitcrawler/parsing.py
@@ -1,11 +1,28 @@
-"""TODO: Docstring
+# -*- coding: utf-8 -*-
+"""Utilities for parsing html.
+
+Extends functionality of BeautifulSoup for added html parsing functionality.
+
 """
 from bs4 import BeautifulSoup
 
 class HtmlParser(BeautifulSoup):
-    """TODO: Docstring
+    """HtmlParser extends functionality provided by BeautifulSoup.
+
     """
     def get_links(self):
-        """TODO: Docstring
+        """Finds links from anchor tags in the soup.
+
+        Args:
+            None
+
+        Returns:
+            list(str): A list of links discovered within the html.
+
+        Examples:
+            >>> response = requests.get("http://python.org")
+            >>> HtmlParser(response.text).get_links()
+            ["http://python.org/search", "/about", ..., "http://python.org/learn"]
+
         """
         return list(set(a['href'] for a in self.find_all('a', href=True)))

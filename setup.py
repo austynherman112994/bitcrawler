5175eacbb03052677a1f5653d36e9e6d077f760c
Austyn Herman <austynherman112994@gmail.com>
2021-03-09 12:42:00 -0800

    v0.1.0 version bump for release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bitcrawler", # Replace with your own username
-    version="0.0.4",
+    version="0.1.0",
     author="Austyn Herman",
     author_email="austynherman112994@gmail.com",
     description="Crawling the web made easy.",
